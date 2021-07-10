bc5724040eaad5660047e75434308861f457bc80
tie <493820064@qq.com>
2021-07-10 19:36:02 +0800

    提交room

--- TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,12 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+}
